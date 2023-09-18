7041cdbc2c9a0f9bcc63de238dd0eccf904ad8ed
Animesh Kumar <animesh@Animeshs-MacBook-Air.local>
2023-09-18 20:35:02 +0530

    final

--- app.py
+++ app.py
@@ -5,28 +5,31 @@ import pickle
 import pandas as pd
 from keras.models import load_model
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-cors=CORS(app)
+cors = CORS(app)
 model = load_model('model.h5')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-
     veg = request.form.get('veg')
-    season =request.form.get('season')
+    season = request.form.get('season')
     temp = request.form.get('temp')
     mon = request.form.get('mon')
     condition = request.form.get('condition')
     disaster = request.form.get('disaster')
-    
-    prediction = model.predict([[int(veg),int(season),int(temp),
-                              int(mon),int(condition),int(disaster)]])
+
+    prediction = model.predict([[int(veg), int(season), int(temp),
+                                  int(mon), int(condition), int(disaster)]])
     print(prediction)
 
-    return str(np.round(prediction[0][0],2))
+    return str(np.round(prediction[0][0], 2))
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
+
